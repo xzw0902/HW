24da02a34870a94353e6b287fc72a10b6f327adf
xzw0902 <wuxiyuan88@gmail.com>
2021-09-01 12:32:37 -0500

    Homework

---
+++ src/main/java/com/example/course/ioc/Starter.java
@@ -0,0 +1,24 @@
+package com.example.course.ioc;
+
+@Component
+
+class Starter {
+
+    @Autowired
+    private static StudentApplication studentApplication;
+
+    @Autowired
+    @Qualifier(name = "StudentRegisterServiceImplement1 ")
+    private static StudentRegisterService studentRegisterServiceImpl1;
+
+    @Qualifier(name = "StudentRegisterServiceImplement2")
+    @Autowired
+    private static StudentRegisterService studentRegisterServiceImpl2;
+
+    public static void main(String [] args) throws Exception {
+        Container.start();
+        System.out.println(studentApplication);
+        System.out.println(studentRegisterServiceImpl1);
+        System.out.println(studentRegisterServiceImpl2);
+    }
+}
